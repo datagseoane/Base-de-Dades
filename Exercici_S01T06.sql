535ed6870988c3a1d21fa3a6e3a50475b1eed54c
Guillermo <seoaneg@gmail.com>
2022-09-20 12:00:19 +0200

    Delete Exercici_S01T06.sql

--- Exercici_S01T06.sql
+++
@@ -1,9 +0,0 @@
--- Exercici 6
------------------------------------------
--- Elimina la pel·lícula "La Gran Familia Española" (11) de la base de dades.
-
-SELECT movie_title
-FROM movies.tb_movie_person V
-JOIN movies.tb_movie M ON V.movie_id = M.movie_id
-JOIN movies.tb_person P ON V.person_id = P.person_id
-WHERE movie_title = 'La Gran Familia Española';
