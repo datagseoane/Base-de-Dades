11538bfeed0286be182a3ff96cb27f8d559aa277
Guillermo <seoaneg@gmail.com>
2022-09-21 12:18:54 +0200

    Update Exercici_S01T02.sql

--- Tasca M1 T01/Exercici_S01T02.sql
+++ Tasca M1 T01/Exercici_S01T02.sql
@@ -3,7 +3,7 @@
 -- Has d'obtenir el nom, el país i la data de naixement d'aquelles persones 
 -- per les quals no consti una data de mort i ordenar les dades de la persona més vella a la persona més jove.
 
-SELECT person_name AS Nom, person_country AS País, person_dob AS data_naixement
+SELECT person_name AS Nom, person_country AS País, person_dob AS "Data_Naixement"
 FROM  movies.tb_person
 WHERE person_dod IS NULL
-ORDER BY person_dob ASC;
+ORDER BY Data_Naixement ASC;
