9b5aa5f93ffa708eaac06615b338090f4bdd2593
Guillermo <seoaneg@gmail.com>
2022-09-21 12:22:42 +0200

    Update Exercici_S01T04.sql

--- Tasca M1 T01/Exercici_S01T04.sql
+++ Tasca M1 T01/Exercici_S01T04.sql
@@ -5,9 +5,9 @@
 
 -- Posteriorment, mostra únicament aquelles persones que hagin assumit més d'un rol en una mateixa pel·lícula.
 
-SELECT person_name AS Nom, movie_id, count(role_id) AS total_rols
+SELECT person_name AS "Nom", movie_id, count(role_id) AS "total_rols"
 FROM movies.tb_person P
 JOIN movies.tb_movie_person V ON P.person_id = V.person_id
 GROUP BY person_name, movie_id
-HAVING count(role_id) > 1
-ORDER BY count(role_id) DESC;
+HAVING total_rols > 1
+ORDER BY total_rols DESC;
