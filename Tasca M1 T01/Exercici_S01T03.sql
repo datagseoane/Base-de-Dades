fb8dda84bcca54b7a1172ee934cdb203741ea527
Guillermo <seoaneg@gmail.com>
2022-09-21 12:21:09 +0200

    Update Exercici_S01T03.sql

--- Tasca M1 T01/Exercici_S01T03.sql
+++ Tasca M1 T01/Exercici_S01T03.sql
@@ -3,8 +3,8 @@
 -- Has d'obtenir el nom del gènere i el nombre total de pel·lícules d'aquest gènere 
 -- i ordenar-ho per ordre descendent de nombre total de pel·lícules. 
 
-SELECT genre_name, count(movie_title) AS total_movies
+SELECT genre_name AS "Nom genre", count(movie_title) AS "Total_movies"
 FROM movies.tb_movie
 JOIN movies.tb_genre ON movie_genre_id = genre_id
 GROUP BY genre_name
-ORDER BY count(movie_title) DESC;
+ORDER BY Total_movies DESC;
